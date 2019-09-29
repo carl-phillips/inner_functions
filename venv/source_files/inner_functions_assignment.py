41014a861ffbdd5f14d6a65ec69e7d590dc381ee
cfphillips2 <36976465+cfphillips2@users.noreply.github.com>
2019-09-29 18:54:21 -0500

    Method is prepared for 2 separate sets of inputs, if theres 1 in the list, it will calculate for a square, if there is 2 in the list, it will calculate for a rectangle

--- venv/source_files/inner_functions_assignment.py
+++ venv/source_files/inner_functions_assignment.py
@@ -5,8 +5,17 @@ def measurements(measurements):
     def perimeter(a_list):
         return 2 * (a_list[0] + a_list[1])
 
-    print("Perimeter = " + str(perimeter(measurements)) + " Area = " + str(area(measurements)))
+    def area_square(a_list):
+        return a_list[0] * a_list[0]
+
+    def perimeter_square(a_list):
+        return 4 * a_list[0]
+
+    if len(measurements) == 1:
+        return ("Perimeter = " + str(perimeter_square(measurements)) + " Area = " + str(area_square(measurements)))
+    elif len(measurements) == 2:
+        return ("Perimeter = " + str(perimeter(measurements)) + " Area = " + str(area(measurements)))
 
 
 if __name__ == '__main__':
-    measurements([2.1, 3.4]) 
+    print(measurements([2.1, 3.4]))
